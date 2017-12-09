6e3a9d1072ee74cd0b31499364bb29d0ca3cabb9
e.alcobaca <edesio@usp.br>
2017-12-09 18:30:14 +0000

    ml module ok

--- scripts/ml.R
+++ scripts/ml.R
@@ -2,7 +2,9 @@ library(mlr)
 
 
 ml = function(data.path, output.path){
-    task = makeClassifTask(data = read.csv2('../datasets/PRAD.mirnaseq.csv'), target = "Class") 
+
+    data = read.csv2(data.path)
+    task = makeClassifTask(data=data, target = "Class") 
 
     ## Two learners to be compared
     lrns = list(makeLearner("classif.svm"), 
@@ -17,9 +19,22 @@ ml = function(data.path, output.path){
     ## Conduct the benchmark experiment
     bmr = benchmark(lrns, task, rdesc, 
         measures=list(acc, bac, f1, gmean, kappa, tpr, tnr), 
-        keep.pred = TRUE, models = TRUE)
+        keep.pred = TRUE, models = TRUE, show.info=FALSE)
+    
+    perf = getBMRAggrPerformances(bmr, as.df = TRUE)[,-1]
+    for(i in 2:ncol(perf)){
+        perf[,i] = round(perf[,i],4)
+    } 
 
+    write.csv2(x=perf, output.path, row.names=FALSE)
+    save(perf, file=paste(output.path,'.models.RData',sep=''))
 
 }
 
+main = function(){
+    line <- readLines(file("stdin"),1)
+    print(line)
+}
 
+
+ml('../input/dataset/in.csv', '../output/ml/out.csv')
